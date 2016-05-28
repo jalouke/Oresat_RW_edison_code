bb47bb03178830b31d8d1c9387ae592506a6d8d1
Marie House <Marie House>
2016-05-27 22:51:35 -0700

    Figured out how to convert array to number

--- testing_1_2_3.py
+++ testing_1_2_3.py
@@ -6,6 +6,16 @@ c_c = 1/(np.cos(53*pi/180)*4) #transformation coefficient #3
 X_velocity=Y_velocity=Z_velocity=1
 Card_to_Motor = [[c_a,c_b,c_c],[c_a,-c_b,c_c],[-c_a,-c_b,c_c],[-c_a,c_b,c_c]] #cubesat reference frame to motor reference frame conversion (3x4)
 Cardinal_axis = [[X_velocity],[Y_velocity],[Z_velocity]]
-[A_Motor_velocity,B_Motor_velocity,C_Motor_velocity,D_Motor_velocity]=np.dot(Card_to_Motor,Cardinal_axis)
 
-print A_Motor_velocity,B_Motor_velocity,C_Motor_velocity,D_Motor_velocity
+[A_Motor_velocity,B_Motor_velocity,C_Motor_velocity,D_Motor_velocity] = np.dot(Card_to_Motor,Cardinal_axis)
+
+alpha = float (A_Motor_velocity)
+bravo = float (B_Motor_velocity)
+charlie = float (C_Motor_velocity)
+delta = float (D_Motor_velocity)
+
+print alpha
+print bravo
+print charlie
+print delta
+
