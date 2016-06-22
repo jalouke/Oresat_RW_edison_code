f5cf388d241d684739210678ca2bcbab0645252c
Marie House <Marie House>
2016-06-21 20:52:13 -0700

    Started commenting code

--- Drop_Test_Bode.py
+++ Drop_Test_Bode.py
@@ -1,28 +1,53 @@
+##This code is written to give the CubeSAT a constant angular velocity about the z-axis##
+
+#"System Management Bus", turning on and off?
 import smbus
-import mraa 
-import time 
+#allows easy interface w/ I/O pins on Edison board
+import mraa
+#time module
+import time
+#math module
 import math
+#to be able to use large arrays and matrices
 import numpy as np
+#?
 import IMU
+#to process command line arguments
 import sys
 
 #########################################################
-# initializing Variables
-Frequency = 1 #frequency of input response in Hz
+#Initializing Variables
+
+#frequency of input response in Hz
+Frequency = 1
+#motor velocities initially set to zero
 A_motor_velocity=B_motor_velocity=C_motor_velocity=D_motor_velocity=0
+#x,y,z velocities initially set to zero
 X_velocity=Y_velocity=Z_velocity=0
+#initialize outputs equal to zero
 A=B=C=D=E=F=G=H=I=0
+#define A-I as an array
 output = ['A','B','C','D','E','F','G','H','I']
-Pin = [14,20,0,21,36,48,47,32,46] #PWM for GP13,GP12,GP182,GP183 Gpio for GP14,GP15,GP49,GP46,GP47
+#Edison pins GP13,GP12,GP182,GP183 as PWM
+#Edison pins GP14,GP15,GP49,GP46,GP47 as GPIO
+Pin = [14,20,0,21,36,48,47,32,46]
 pi = 3.1415926
-c_a = np.sin(45*pi/180)/2 #transformation coefficient #1
-c_b = np.sin(45*pi/180)/2 #transformation coefficient #2
-c_c = 1/(np.cos(53*pi/180)*4) #transformation coefficient #3
-Card_to_Motor = [[c_a,c_b,c_c],[c_a,-c_b,c_c],[-c_a,-c_b,c_c],[-c_a,c_b,c_c]] #cubesat reference frame to motor reference frame conversion (3x4)
+#transformation coefficient #1
+c_a = np.sin(45*pi/180)/2
+#transformation coefficient #2
+c_b = np.sin(45*pi/180)/2
+#transformation coefficient #3
+c_c = 1/(np.cos(53*pi/180)*4)
+#cubesat reference frame to motor reference frame conversion (3x4)
+Card_to_Motor = [[c_a,c_b,c_c],[c_a,-c_b,c_c],[-c_a,-c_b,c_c],[-c_a,c_b,c_c]]
+#?
 phase=0
+#time initially equal to zero
 t=0
+
 ##########################################################
-# enabling outputs 
+
+#Enabling Outputs 
 for x in xrange(0,4):
 	output[x] = mraa.Pwm(Pin[x])
 	output[x].period_us(700)
